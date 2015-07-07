e4e55862b162ec8de54833538aa6feab7435dfea
Simon Holmes <simonholmes@8hflvz1.chep.com>
2015-07-07 18:15:25 +0100

    throttle

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -14,7 +14,7 @@ let NavBar = React.createClass({
 			<div className='navBar navbar-fixed'>
 				<nav>
 					<div class="nav-wrapper">
-						<a className="brand-logo menu-icon">
+						<a onClick={this.menuClick} className="brand-logo menu-icon">
 							<svg viewBox="0 0 24 24">
 								<path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"></path>
 							</svg>
--- src/components/search.jsx
+++ src/components/search.jsx
@@ -7,30 +7,29 @@ import { throttle } from '../modules';
 
 import { SearchResults } from './'
 
-let Search = React.createClass({
-  
-  _handleInputChange(event) {
-  	console.log('Handle input chnage');
-    let query = event.target.value;
-    throttle(Actions.searchYoutubeApi(query), 3000);
-    throttle(console.log(query), 3000);
-  },
+function handleInputChange(event) {
+  console.log('Handle input chnage');
+  let query = event.target.value;
+  Actions.searchYoutubeApi(query);
+};
 
+let Search = React.createClass({
+
+  componentWillMount() {
+    this.onChange = throttle(handleInputChange, 1000);
+  },
+  
   overlayClick() {
   	console.log('overlay click');
   	Actions.toggleSearch(true);
   },
 
   render() {
-  	 //    window.addEventListener("resize", function(){console.log('resize no throttle')});
-    // window.addEventListener("resize", throttle( function(){console.log('rsize throttle')}, 2000 ));
-
     return (
     	<div className={this.props.open ? 'searchWrapper open' : 'searchWrapper'}>
 	      <div className='search' >
-	        <h3>Search...</h3>
 	        <div class="input-field col s6">
-            <input placeholder="Search youtube" onChange={this._handleInputChange} type="text" class="validate" refs="searchBar"/>
+            <input placeholder="Search youtube" onChange={this.onChange} type="text" class="validate" refs="searchBar"/>
             <label for="first_name">Search youtube</label>
           </div>
 	        <SearchResults results={this.props.results}/>
